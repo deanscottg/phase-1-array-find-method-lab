bad281ac42badbf260e6594af3d2fefe75a6eb09
Dean <deanscottg@gmail.com>
2022-02-03 17:10:19 -0500

    commit tree 5fc4758e5ee14f29e9fc2aef87e053e85e21c2dc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+// function superbowlWin(record){
+//     for (const item of record) if(
+//         item.result === "W"){
+//             return item.year
+//         }
+//     }
+// let superbowlWin= record.find(win)
+// function win(super)
+function superbowlWin(record){
+    let winner = record.find((record) => record.result === "W");
+    if (!winner) return undefined;
+    else return winner.year;
+}
